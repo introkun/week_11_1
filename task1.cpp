b95e8358e0606ecda604531c071a1ce86d1ee7c4
setlol <dovnar.maxim@gmail.com>
2016-08-23 16:21:05 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int yearLeap(int(&dataStart)[3], int(&dataStop)[3]);
+int daysMonth(int(&dataStart)[3], int(&dataStop)[3]);
+int day(int(&dataStart)[3], int(&dataStop)[3]);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int const size = 3;
+	int dataStart[size];
+	int dataStop[size];
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (j == 0)
+			{
+				cout << "введите год";
+			}
+			else if (j == 1)
+			{
+				cout << "введите месяц";
+			}
+			else
+			{
+				cout << "вветеде день";
+			}
+			if (i == 0)
+			{
+				cin >> dataStart[j];
+			}
+			else
+			{
+				cin >> dataStop[j];
+			}
+		}
+	}
+	int result = day(dataStart, dataStop);
+	cout << result << endl;
+}
+
+int day(int(&dataStart)[3], int(&dataStop)[3])
+{
+	int result;
+	int year = yearLeap(dataStart, dataStop);
+	int month = daysMonth(dataStart, dataStop);
+	int day = dataStart[2] + dataStop[2];
+	result = year - (month - day);
+	return result;
+}
+int yearLeap(int(&dataStart)[3], int(&dataStop)[3])
+{
+	int result = 0;
+	for (; dataStart[0] <= dataStop[0]; dataStart[0]++)
+	{
+		if (dataStart[0] % 4 == 0 &&
+			dataStart[0] % 100 != 0 ||
+			dataStart[0] % 400 == 0)
+		{
+			result += 366;
+		}
+		else
+		{
+			result += 365;
+		}
+	}
+	return result;
+}
+
+int daysMonth(int(&dataStart)[3], int(&dataStop)[3])
+{
+	int result = 0;
+	int l = 0;
+	int i = 0;
+	for (int j = 0; j < 2; j++)
+	{
+		if (j = 0)
+		{
+			l = dataStart[1];
+			i = 1;
+		}
+		else
+		{
+			l = 12;
+			i = dataStop[1];
+		}
+
+		for (; i <= l; i++)
+		{
+			if (i == 4 ||
+				i == 6 ||
+				i == 9 ||
+				i == 11)
+			{
+				result += 30;
+			}
+			else if (i == 2 &&
+				dataStart[1] % 4 == 0 &&
+				dataStart[1] % 100 != 0 ||
+				i == 2 &&
+				dataStart[1] % 400 == 0)
+			{
+				result += 29;
+			}
+			else if (i == 2 &&
+				dataStart[1] % 4 != 0 &&
+				dataStart[1] % 100 == 0 ||
+				i == 2 &&
+				dataStart[1] % 400 != 0)
+			{
+				result += 28;
+			}
+			else
+			{
+				result += 31;
+			}
+		}
+	}
+	return result;
+}
---
+++ task2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int average(int(&arr)[10]);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int const size = 10;
+	int arr[size];
+	for (int i = 0; i < size; i++)
+	{
+		cout << "введите значение " << i + 1 << " = ";
+		cin >> arr[i];
+	}
+	cout << average(arr) << endl;
+}
+
+int average(int(&arr)[10])
+{
+	int const size = 10;
+	int result = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		result += arr[i];
+	}
+	result /= size;
+	return result;
+}
